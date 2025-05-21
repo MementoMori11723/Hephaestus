991df0ed165707a74492428bfd107460215b7b91
yasasvi <yasasvigumma@gmail.com>
2025-05-21 17:28:20 +0530

    #6 Keeping it simple!
    Removed too many pages, and focuing on nessary data!

--- components/__init__.py
+++ components/__init__.py
@@ -1,9 +1,7 @@
-__all__ = ["common", "about", "places", "home", "personalize", "clothes", "flavors"]
+__all__ = ["common", "about", "home", "personalize", "explore"]
 
 from . import common
 from . import about
-from . import places
 from . import home
 from . import personalize
-from . import clothes
-from . import flavors
+from . import explore
