30ef13546463410656960537085be78569982cb3
yasasvi <yasasvigumma@gmail.com>
2025-05-25 18:10:26 +0530

    #13 Moved all files to .db file!
    Also removed base.py so we can use api to do all of work...

--- data/base.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-# def store_data(data:dict):
-#     for name, df in zip(data.keys(), data.values()):
-#         with sqlite3.connect("data/db/base.db") as conn:
-#             df.to_sql(name.replace(".csv", ""), conn, if_exists="replace", index=False)
-#     print("Added all files to .db")
