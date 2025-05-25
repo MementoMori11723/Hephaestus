4bb60041fb4de70bf49b82405036866cd654aa55
yasasvi <yasasvigumma@gmail.com>
2025-05-25 18:06:10 +0530

    #12 Removed a csv file!
    we don't need temperature...

--- components/culture.py
+++ components/culture.py
@@ -1,13 +1,4 @@
-from data import analysis
-from data.analysis import load_data
 from streamlit.delta_generator import DeltaGenerator
 
 def Culture(main:DeltaGenerator) -> None:
-    # datasets = load_data()
     main.header("Datasets")
-    # for name, data  in zip(datasets.keys(), datasets.values()):
-    #     main.write(name)
-    #     main.dataframe(data)
-    #     main.divider()
-    data = analysis.analyse()
-    main.dataframe(data)
--- data/analysis.py
+++ data/analysis.py
@@ -1,22 +1,4 @@
-import os
-import pandas as pd
 # from data.api import fetch
 
-def load_data() -> dict[str, pd.DataFrame]:
-    data = {}
-    
-    for file in os.listdir("datasets/"):
-        data[file] = pd.read_csv(
-            os.path.join("datasets/", file)
-        )
-
-    return data
-
 def analyse():
-
-    data = load_data()
-
-    df = data["Crop_production.csv"]
-
-    return df
-
+    pass
